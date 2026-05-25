9690ebd52368b57cdb1bc180c52af31309312f3e
Robby baehaqi <Robby.baehaqi32@gmail.com>
2026-05-25 20:36:02 +0700

    Fix: Correct tabler-icons-webfont URL path for version 3.44.0 (#54)

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,7 +11,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/core@1.4.0/dist/css/tabler.min.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@3.44.0/tabler-icons.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@3.44.0/dist/tabler-icons.min.css">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="d-flex flex-column">
