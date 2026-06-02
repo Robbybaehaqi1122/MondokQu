4df9408bf140eb796ee017897b8c36d878fb5479
Robbybaehaqi1122 <Robby.baehaqi32@gmail.com>
2026-06-02 15:25:13 +0700

    fix: hapus CSS override all anchor tags jadi theme color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,6 @@
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/core@1.4.0/dist/css/tabler.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@3.44.0/dist/tabler-icons.min.css">
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         @php
             $themeColor = config('app.tenant_theme_color', '#206bc4');
@@ -42,14 +41,13 @@
                 border-color: {{ $themeColor }};
                 color: #fff;
             }
-            a, .link-primary, .text-primary {
-                color: {{ $themeColor }} !important;
-            }
             .sidebar-link.active, .sidebar-sublink.active,
             .sidebar-dropdown[open] > .sidebar-link {
                 color: {{ $themeColor }};
             }
         </style>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.4.0/dist/js/tabler.min.js"></script>
